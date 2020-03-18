30350a5e10e8a30aba2a9079ef5984eac5d5e202
Alex Kukla <alex.kukla@petersons.com>
2020-03-18 17:57:34 +0200

    Remove debug info from run file

--- run.php
+++ run.php
@@ -5,8 +5,5 @@ require __DIR__ . '/vendor/autoload.php';
 use App\Sources\CommandLineSource;
 use App\Calculator;
 
-var_dump($argv[1]);
-die();
-
 $calculator = new Calculator(new CommandLineSource());
 $calculator->run();
